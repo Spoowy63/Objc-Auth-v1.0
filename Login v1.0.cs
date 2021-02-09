cd3e526391d8fef1835af80a518b7ea192ee317e
Spoowy <78769981+Spoowy63@users.noreply.github.com>
2021-02-09 14:45:47 +0100

    Update Login v1.0.cs

--- Login v1.0.cs
+++ Login v1.0.cs
@@ -1,5 +1,7 @@
 static void didFinishLaunching(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef info) {
 
+#define timer(sec) dispatch_after(dispatch_time(DISPATCH_TIME_NOW, sec * NSEC_PER_SEC), dispatch_get_main_queue(), ^
+
 NSUserDefaults *check;
 check = [NSUserDefaults standardUserDefaults];
 
